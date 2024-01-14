2955d8bad77d172e9ea9dacf382c0c969faa70de
Tomas Castellano <tomas@itaptec.com.ar>
2024-01-14 11:36:16 -0300

    metricas y readme
    Co-authored-by: EzequielLassalle <EzequielLassalle@users.noreply.github.com>

---
+++ tp1/Readme.md
@@ -0,0 +1,13 @@
+# TP1
+| Integrantes       | Padron |
+| ------------------| ------ |
+| Martín Tomás      | 100835 |
+| Adriana Macarena  |        |
+  Iglesias Tripodi  | 103384 |
+| Ezequiel Lassalle | 105801 |
+
+## Ejecutar aplicación
+Para ejecutar la aplicacion, posicionese en la raiz del proyecto, abra una consola y ejecute el siguiente script.
+```
+python greedy.py
+```
--- tp1/greedy.py
+++ tp1/greedy.py
@@ -1,18 +1,57 @@
-tiempoScaloniAyudantes = [(3, 200), (10, 195), (2, 19), (20, 38), (3, 1), (15, 1)]
-tiempoScaloniAyudantes.sort(key=lambda tup: tup[1], reverse=True)
+import random
+import time
 
-tiempoEnFinalizarAyudante = []
-tiempoAcumuladoScaloni = 0
+def calcularTiempoMaximo(n, tiempoScaloni, tiempoAyudantes, debug=True, showElapsedTime=False):
+    if (n <= 0):
+        return
+    
+    start = time.perf_counter()
+    #plotTime=[]
+    tiempoScaloniAyudantes = []
+    for i in range(0, n):
+         tiempoScaloniAyudantes.append((tiempoScaloni[i], tiempoAyudantes[i]))
+    
+    # Complejidad O(nlogn)
+    tiempoScaloniAyudantes.sort(key=lambda tup: tup[1], reverse=True)
 
-for tiempo in tiempoScaloniAyudantes:
-    tiempoScaloni = tiempo[0]
-    tiempoAyudante = tiempo[1]
+    tiempoEnFinalizarAyudante = []
+    tiempoAcumuladoScaloni = 0
 
-    tiempoEnFinalizarAyudante.append(tiempoScaloni + tiempoAyudante + tiempoAcumuladoScaloni)
+    # Complejidad O(n)
+    for tiempo in tiempoScaloniAyudantes:
+        tiempoScaloni = tiempo[0]
+        tiempoAyudante = tiempo[1]
 
-    tiempoAcumuladoScaloni += tiempoScaloni
+        tiempoEnFinalizarAyudante.append(tiempoScaloni + tiempoAyudante + tiempoAcumuladoScaloni)
 
-print("Lo que demora cada ayudante en terminar de ver su video es: ")
-print(tiempoEnFinalizarAyudante)
+        tiempoAcumuladoScaloni += tiempoScaloni
 
-print("El tiempo que se demora terminar de ver todos los videos es: ", max(tiempoEnFinalizarAyudante))
+    end = time.perf_counter()
+
+    if (showElapsedTime):
+        print((n, end - start))
+        #plotTime.append(n,end - start)
+
+    if (debug):
+        print("Lo que demora cada ayudante en terminar de ver su video es: ")
+        print(tiempoEnFinalizarAyudante)
+
+        print("El tiempo que se demora terminar de ver todos los videos es: ", max(tiempoEnFinalizarAyudante))
+
+#Tests
+videosAMirarScaloni = []
+videosAMirarAyudantes = []
+
+
+for n in range(1, 500):
+    videosAMirarScaloni.append(random.randint(1, 25))
+    videosAMirarAyudantes.append(random.randint(1, 50))
+    
+    
+    calcularTiempoMaximo(
+        n, 
+        videosAMirarScaloni, 
+        videosAMirarAyudantes, 
+        debug=False, 
+        showElapsedTime=True
+    )
