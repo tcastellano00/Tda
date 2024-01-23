117e8bf08440f778920b13fb756058a90bf5bc50
AIglesiasT <105086056+AIglesiasT@users.noreply.github.com>
2024-01-22 22:32:11 -0300

    Update greedy.py

--- tp1/greedy.py
+++ tp1/greedy.py
@@ -1,12 +1,12 @@
 import random
 import time
+import matplotlib.pyplot as plt
+import numpy as np
 
-def calcularTiempoMaximo(n, tiempoScaloni, tiempoAyudantes, debug=True, showElapsedTime=False):
+def calcularTiempoMaximo(n, tiempoScaloni, tiempoAyudantes,debug):
     if (n <= 0):
         return
     
-    start = time.perf_counter()
-    #plotTime=[]
     tiempoScaloniAyudantes = [list(x) for x in zip(tiempoScaloni,tiempoAyudantes)]
     
     # Complejidad O(nlogn)
@@ -14,42 +14,62 @@ def calcularTiempoMaximo(n, tiempoScaloni, tiempoAyudantes, debug=True, showElap
 
     tiempoEnFinalizarAyudante = []
     tiempoAcumuladoScaloni = 0
-
+    tiempoMaximo = 0
     # Complejidad O(n)
     for tiempo in tiempoScaloniAyudantes:
         tiempoScaloni = tiempo[0]
         tiempoAyudante = tiempo[1]
 
-        tiempoEnFinalizarAyudante.append(tiempoScaloni + tiempoAyudante + tiempoAcumuladoScaloni)
-
+        #tiempoEnFinalizarAyudante.append(tiempoScaloni + tiempoAyudante + tiempoAcumuladoScaloni)
         tiempoAcumuladoScaloni += tiempoScaloni
-
-    end = time.perf_counter()
-
-    if (showElapsedTime):
-        print((n, end - start))
-        #plotTime.append(n,end - start)
-
-    if (debug):
-        print("Lo que demora cada ayudante en terminar de ver su video es: ")
-        print(tiempoEnFinalizarAyudante)
-
-        print("El tiempo que se demora terminar de ver todos los videos es: ", max(tiempoEnFinalizarAyudante))
-
-#Tests
+        tiempoMaximo=max(tiempoAcumuladoScaloni+tiempoAyudante, tiempoMaximo)
+    if (debug):        
+        print("El tiempo que se demora terminar de ver todos los videos es: ", tiempoMaximo, tiempoMaximo2)
+        
 videosAMirarScaloni = []
 videosAMirarAyudantes = []
 
 
-for n in range(1, 500):
-    videosAMirarScaloni.append(random.randint(1, 25))
-    videosAMirarAyudantes.append(random.randint(1, 50))
-    
-    
-    calcularTiempoMaximo(
-        n, 
-        videosAMirarScaloni, 
-        videosAMirarAyudantes, 
-        debug=False, 
-        showElapsedTime=True
-    )
+x=np.arange(0,100000,1000)
+#x=np.arange(0,8,1)
+#x=10**x
+y=[]
+#rng = np.random.default_rng()
+for n in x:
+    #start =time.perf_counter()
+    mean_start=[]
+    mean_end=[]
+    for i in range(1, 5):
+        #videosAMirarScaloni=rng.integers(300,size=(1,n*10))
+        #videosAMirarAyudantes=rng.integers(400,size=(1,n*10))
+        #start=time.perf_counter()
+        videosAMirarScaloni = [random.uniform(1, 300) for _ in range(n)]
+        videosAMirarAyudantes = [random.uniform(1, 400) for _ in range(n)]
+        mean_start.append(time.perf_counter())
+        calcularTiempoMaximo(
+            n, 
+            videosAMirarScaloni, 
+            videosAMirarAyudantes,
+            debug=True
+        )
+        mean_end.append(time.perf_counter())
+    start=np.mean(mean_start)
+    end=np.mean(mean_end)
+    #end=time.perf_counter()
+    y.append(end-start)
+
+
+# Crear el gráfico
+plt.figure(figsize=(20, 8))
+plt.plot(x, y,marker='o',label = "nuestro")
+#plt.plot(x, (x*np.log(x))/(10**3),label = "n log(n)")
+plt.xlabel('Cantidad de videos visualizados', fontsize=16)
+plt.ylabel('Tiempo de Implementación [s]', fontsize=16)
+plt.title('Cantidad de Videos Vs Tiempo de Implementación', fontsize=26)
+plt.legend()
+plt.grid(True)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+
+# Mostrar el gráfico
+plt.show()
